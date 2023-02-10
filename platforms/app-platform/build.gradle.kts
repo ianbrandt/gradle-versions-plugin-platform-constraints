ee0e4a7d810514f7de2339ebb1a8c45e5b8efa18
Ian Brandt <ian@ianbrandt.com>
2023-02-10 08:36:53 -0800

    Tried ComponentMetadataRule only in the convention plugin.

--- platforms/app-platform/build.gradle.kts
+++ platforms/app-platform/build.gradle.kts
@@ -11,8 +11,6 @@ javaPlatform {
 
 dependencies {
 
-	components.all<SpringBomAlignmentRule>()
-
 	api(platform(libs.kotlinx.coroutines.bom))
 	api(platform(libs.spring.bom))
 	api(platform(libs.spring.boot.bom))
@@ -20,24 +18,5 @@ dependencies {
 	constraints {
 		api(libs.jetbrains.annotations)
 		api(libs.bundles.kotlinx.datetime.jvm)
-	}
-}
-
-abstract class SpringBomAlignmentRule : ComponentMetadataRule {
-	override fun execute(ctx: ComponentMetadataContext) {
-		ctx.details.run {
-			when (id.group) {
-				"org.springframework" ->
-					belongsTo(
-						"org.springframework:spring-framework-bom:${id.version}",
-						false
-					)
-				"org.springframework.boot" ->
-					belongsTo(
-						"org.springframework.boot:spring-boot-dependencies:${id.version}",
-						false
-					)
-			}
-		}
 	}
 }
